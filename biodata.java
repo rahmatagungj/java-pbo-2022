e2e236ee6318aa8dacbdc7a5a24af1973bf90a47
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-01-13 16:14:04 +0700

    fix typo

--- biodata.java
+++ biodata.java
@@ -1,6 +1,6 @@
 class biodata {
   public static void main(String[] args) {
-    String nama = "Rahmat Agung Jlians";
+    String nama = "Rahmat Agung Julians";
     String nim = "191223045";
     String prodi = "Pendidikan Teknologi Informasi dan Komputer";
     String kampus = "Sekolah Tinggi Keguruan dan Ilmu Pendidikan Muhammadiyah Kuningan";
